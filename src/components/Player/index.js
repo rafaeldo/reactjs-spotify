9fbc0484e86820c10089064fcd84fc357724340f
Rafael de Oliveira <rafaextradev@gmail.com>
2019-03-24 18:15:14 -0300

    update: dashboard and controls layout.

---
+++ src/components/Player/index.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import Slider from 'rc-slider';
+
+import {
+  Container, Current, Volume, Progress, Controls, Time, ProgressSlider,
+} from './styles';
+
+import VolumeIcon from '../../assets/images/volume.svg';
+import ShuffleIcon from '../../assets/images/shuffle.svg';
+import BackwardIcon from '../../assets/images/backward.svg';
+import PlayIcon from '../../assets/images/play.svg';
+// import PauseIcon from '../../assets/images/pause.svg';
+import ForwardIcon from '../../assets/images/forward.svg';
+import RepeatIcon from '../../assets/images/repeat.svg';
+
+const Player = () => (
+  <Container>
+    <Current>
+      <img src="" alt="" />
+
+      <div>
+        <span>Times Like These</span>
+        <small>Foo Fighters</small>
+      </div>
+    </Current>
+
+    <Progress>
+      <Controls>
+        <button>
+          <img src={ShuffleIcon} alt="Aleatório" />
+        </button>
+
+        <button>
+          <img src={BackwardIcon} alt="Anterior" />
+        </button>
+
+        <button>
+          <img src={PlayIcon} alt="Tocar" />
+        </button>
+
+        <button>
+          <img src={ForwardIcon} alt="Próxima" />
+        </button>
+
+        <button>
+          <img src={RepeatIcon} alt="Repetir" />
+        </button>
+      </Controls>
+
+      <Time>
+        <span>1:30</span>
+        <ProgressSlider>
+          <Slider
+            railStyle={{ background: '#404040', borderRadius: 10 }}
+            trackStyle={{ background: '#1ed760' }}
+            handleStyle={{ border: 0 }}
+          />
+        </ProgressSlider>
+        <span>5:30</span>
+      </Time>
+    </Progress>
+
+    <Volume>
+      <img src={VolumeIcon} alt="Volume" />
+      <Slider
+        railStyle={{ background: '#404040', borderRadius: 10 }}
+        trackStyle={{ background: '#fff' }}
+        handleStyle={{ display: 'none' }}
+        value={100}
+      />
+    </Volume>
+  </Container>
+);
+
+export default Player;
