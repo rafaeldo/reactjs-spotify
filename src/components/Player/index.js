e699a1f7e8afd1924e832f2f67ea17cf9d83045a
Rafael de Oliveira <rafaextradev@gmail.com>
2019-03-24 19:12:52 -0300

    add: routes structure and main content.

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -16,7 +16,10 @@ import RepeatIcon from '../../assets/images/repeat.svg';
 const Player = () => (
   <Container>
     <Current>
-      <img src="" alt="" />
+      <img
+        src="https://img.discogs.com/TV3Wkv9wr-l9aJUxQ-QB-m420YQ=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1243297-1393304135-4550.jpeg.jpg"
+        alt="Capa do Album"
+      />
 
       <div>
         <span>Times Like These</span>
---
+++ src/pages/browse/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {
+  Container, Title, List, Playlist,
+} from './styles';
+
+const Browse = () => (
+  <Container>
+    <Title>Navegar</Title>
+
+    <List>
+      <Playlist href="">
+        <img
+          src="https://img.discogs.com/TV3Wkv9wr-l9aJUxQ-QB-m420YQ=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1243297-1393304135-4550.jpeg.jpg"
+          alt="Capa do Album"
+        />
+        <strong>Rock dos bons</strong>
+        <p>Relaxe enquanto você programa ouvindo apenas as melhores do rock mundial</p>
+      </Playlist>
+    </List>
+  </Container>
+);
+
+export default Browse;
