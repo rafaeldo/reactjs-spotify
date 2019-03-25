a34da3be2642581b0a11e220f80f1eb4c7862081
Rafael de Oliveira <rafaextradev@gmail.com>
2019-03-25 14:23:50 -0300

    remove: reactotron setup

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Fragment } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 
-import './config/reactotron';
-
 import { Wrapper, Container, Content } from './styles/components';
 
 import Sidebar from './components/Sidebar';
